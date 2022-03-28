f1c9652162092fc5a1dccf1feba1f3e1eecdde57
Eugene Elantsev <eugene_el@mail.ru>
2022-03-28 17:26:56 +0500

    correct tabs

--- separate_tables.sql
+++ separate_tables.sql
@@ -3,36 +3,36 @@ create schema gbsqlpractice;
 
 -- Create tables 
 create table gbsqlpractice.persons (
-	persons_id 			int not null primary key,
-    person_full_name	varchar(100),
-    person_birth_date	date,
-    person_status		bool,
-    person_comment		varchar(100)
+	persons_id 		int not null primary key,
+	person_full_name	varchar(100),
+	person_birth_date	date,
+	person_status		bool,
+	person_comment		varchar(100)
 );
 
 create table gbsqlpractice.addresses (
-	addresses_id			int not null primary key auto_increment,
-    addresses_person_id		int,
-    addresses_address		varchar(100),
-    addresses_comment		varchar(100),
-    foreign key (addresses_person_id) references gbsqlpractice.persons (persons_id) on delete set null
+	addresses_id		int not null primary key auto_increment,
+	addresses_person_id	int,
+	addresses_address	varchar(100),
+	addresses_comment	varchar(100),
+	foreign key (addresses_person_id) references gbsqlpractice.persons (persons_id) on delete set null
 );
   
 create table gbsqlpractice.phones (
-	phones_id			int not null primary key auto_increment,
-    phones_person_id	int,
-    phones_phone		int,
-    phones_comment		varchar(100),
-    foreign key (phones_person_id) references gbsqlpractice.persons (persons_id) on delete set null
+	phones_id		int not null primary key auto_increment,
+	phones_person_id	int,
+	phones_phone		int,
+	phones_comment		varchar(100),
+	foreign key (phones_person_id) references gbsqlpractice.persons (persons_id) on delete set null
 );
 
 -- Insert data
 insert gbsqlpractice.persons (
 	persons_id,
-    person_full_name,
-    person_birth_date,
-    person_status,
-    person_comment
+	person_full_name,
+	person_birth_date,
+	person_status,
+	person_comment
 )
 values
 (1, 'Иванов И.И', 19900212, true, 'no comment'),
@@ -53,9 +53,9 @@ values
 (16, 'Кузьмин К.К.', 20200521, false, 'no comment');
 
 insert gbsqlpractice.addresses (
-    addresses_person_id,
-    addresses_address,
-    addresses_comment
+	addresses_person_id,
+	addresses_address,
+	addresses_comment
 )
 values
 (1, 'Можга', 'Место рождения'),
@@ -72,9 +72,9 @@ values
 (10, 'Хабаровск', 'Место рождения');
 
 insert gbsqlpractice.phones (
-    phones_person_id,
-    phones_phone,
-    phones_comment
+	phones_person_id,
+	phones_phone,
+	phones_comment
 )
 values
 (1, 123, 'личный'),
